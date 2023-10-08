cfa41f1cf65ec970c7f1fd13188d016fbd83f8ae
Eleni Mandana <elemandana@gmail.com>
2023-10-08 19:37:41 +0300

    dff

---
+++ codemr/buzzquizworld-javagods 3/html/html_report_buzz/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"buzzquizworld-javagods working set: {Buzz}", "Number of Highly Problematic Classes":0, "Number of Entities":5, "Number of Problematic Classes":0, "Class Lines of Code":284, "Number of External Packages":3, "Number of Packages":1, "Number of External Entities":10}
